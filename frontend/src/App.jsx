734567e75931eebd0b803c04e31a3b73c814e11f
eminbasbayan <emin.basbayan@icloud.com>
2023-09-15 18:58:31 +0300

    header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Hello</h1>
     </div>
   );
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,213 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="global-notification">
+        <div className="container">
+          <p>
+            SUMMER SALE FOR ALL SWIM SUITS AND FREE EXPRESS INTERNATIONAL
+            DELIVERY - OFF 50%!
+            <a href="shop.html"> SHOP NOW</a>
+          </p>
+        </div>
+      </div>
+      <div className="header-row">
+        <div className="container">
+          <div className="header-wrapper">
+            <div className="header-mobile">
+              <i className="bi bi-list" id="btn-menu"></i>
+            </div>
+            <div className="header-left">
+              <a href="index.html" className="logo">
+                LOGO
+              </a>
+            </div>
+            <div className="header-center" id="sidebar">
+              <nav className="navigation">
+                <ul className="menu-list">
+                  <li className="menu-list-item">
+                    <a href="index.html" className="menu-link active">
+                      Home
+                      <i className="bi bi-chevron-down"></i>
+                    </a>
+                    <div className="menu-dropdown-wrapper">
+                      <ul className="menu-dropdown-content">
+                        <li>
+                          <a href="#">Home Clean</a>
+                        </li>
+                        <li>
+                          <a href="#">Home Collection</a>
+                        </li>
+                        <li>
+                          <a href="#">Home Minimal</a>
+                        </li>
+                        <li>
+                          <a href="#">Home Modern</a>
+                        </li>
+                        <li>
+                          <a href="#">Home Parallax</a>
+                        </li>
+                        <li>
+                          <a href="#">Home Strong</a>
+                        </li>
+                        <li>
+                          <a href="#">Home Style</a>
+                        </li>
+                        <li>
+                          <a href="#">Home Unique</a>
+                        </li>
+                        <li>
+                          <a href="#">Home RTL</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li className="menu-list-item megamenu-wrapper">
+                    <a href="shop.html" className="menu-link">
+                      Shop
+                      <i className="bi bi-chevron-down"></i>
+                    </a>
+                    <div className="menu-dropdown-wrapper">
+                      <div className="menu-dropdown-megamenu">
+                        <div className="megamenu-links">
+                          <div className="megamenu-products">
+                            <h3 className="megamenu-products-title">
+                              Shop Style
+                            </h3>
+                            <ul className="megamenu-menu-list">
+                              <li>
+                                <a href="#">Shop Standard</a>
+                              </li>
+                              <li>
+                                <a href="#">Shop Full</a>
+                              </li>
+                              <li>
+                                <a href="#">Shop Only Categories</a>
+                              </li>
+                              <li>
+                                <a href="#">Shop Image Categories</a>
+                              </li>
+                              <li>
+                                <a href="#">Shop Sub Categories</a>
+                              </li>
+                              <li>
+                                <a href="#">Shop List</a>
+                              </li>
+                              <li>
+                                <a href="#">Hover Style 1</a>
+                              </li>
+                              <li>
+                                <a href="#">Hover Style 2</a>
+                              </li>
+                              <li>
+                                <a href="#">Hover Style 3</a>
+                              </li>
+                            </ul>
+                          </div>
+                          <div className="megamenu-products">
+                            <h3 className="megamenu-products-title">
+                              Filter Layout
+                            </h3>
+                            <ul className="megamenu-menu-list">
+                              <li>
+                                <a href="#">Sidebar</a>
+                              </li>
+                              <li>
+                                <a href="#">Filter Side Out</a>
+                              </li>
+                              <li>
+                                <a href="#">Filter Dropdown</a>
+                              </li>
+                              <li>
+                                <a href="#">Filter Drawer</a>
+                              </li>
+                            </ul>
+                          </div>
+                          <div className="megamenu-products">
+                            <h3 className="megamenu-products-title">
+                              Shop Loader
+                            </h3>
+                            <ul className="megamenu-menu-list">
+                              <li>
+                                <a href="#">Shop Pagination</a>
+                              </li>
+                              <li>
+                                <a href="#">Shop Infinity</a>
+                              </li>
+                              <li>
+                                <a href="#">Shop Load More</a>
+                              </li>
+                              <li>
+                                <a href="#">Cart Modal</a>
+                              </li>
+                              <li>
+                                <a href="#">Cart Drawer</a>
+                              </li>
+                              <li>
+                                <a href="#">Cart Page</a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="megamenu-single">
+                          <a href="#">
+                            <img src="img/mega-menu.jpg" alt="" />
+                          </a>
+                          <h3 className="megamenu-single-title">
+                            JOIN THE LAYERING GANG
+                          </h3>
+                          <h4 className="megamenu-single-subtitle">
+                            Suspendisse faucibus nunc et pellentesque
+                          </h4>
+                          <a
+                            href="#"
+                            className="megamenu-single-button btn btn-sm"
+                          >
+                            Shop Now
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="menu-list-item">
+                    <a href="blog.html" className="menu-link">
+                      Blog
+                    </a>
+                  </li>
+                  <li className="menu-list-item">
+                    <a href="contact.html" className="menu-link">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <i className="bi-x-circle" id="close-sidebar"></i>
+            </div>
+            <div className="header-right">
+              <div className="header-right-links">
+                <a href="account.html" className="header-account">
+                  <i className="bi bi-person"></i>
+                </a>
+                <button className="search-button">
+                  <i className="bi bi-search"></i>
+                </button>
+                <a href="#">
+                  <i className="bi bi-heart"></i>
+                </a>
+                <div className="header-cart">
+                  <a href="cart.html" className="header-cart-link">
+                    <i className="bi bi-bag"></i>
+                    <span className="header-cart-count">0</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
