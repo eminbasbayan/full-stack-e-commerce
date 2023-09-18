8666d2e8b35d4ecb0c92811fba0401222e3df6cb
eminbasbayan <emin.basbayan@icloud.com>
2023-09-18 23:33:02 +0300

    react ile slider yapımı

--- frontend/src/components/Slider/SliderItem.jsx
+++ frontend/src/components/Slider/SliderItem.jsx
@@ -1,8 +1,10 @@
-const SliderItem = () => {
+import PropTypes from "prop-types";
+
+const SliderItem = ({ imageSrc }) => {
   return (
     <div className="slider-item fade">
       <div className="slider-image">
-        <img src="img/slider/slider1.jpg" className="img-fluid" alt="" />
+        <img src={imageSrc} className="img-fluid" alt="" />
       </div>
       <div className="container">
         <p className="slider-title">SUMMER 2022</p>
@@ -16,3 +18,7 @@ const SliderItem = () => {
 };
 
 export default SliderItem;
+
+SliderItem.propTypes = {
+  imageSrc: PropTypes.string,
+};
--- frontend/src/components/Slider/Sliders.jsx
+++ frontend/src/components/Slider/Sliders.jsx
@@ -1,27 +1,49 @@
+import { useState } from "react";
 import SliderItem from "./SliderItem";
-import "./Sliders.css"
+import "./Sliders.css";
 
 const Sliders = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % 3);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + 3) % 3);
+  };
+
   return (
     <section className="slider">
       <div className="slider-elements">
-        <SliderItem />
+        {currentSlide === 0 && <SliderItem imageSrc="img/slider/slider1.jpg" />}
+        {currentSlide === 1 && <SliderItem imageSrc="img/slider/slider2.jpg" />}
+        {currentSlide === 2 && <SliderItem imageSrc="img/slider/slider3.jpg" />}
         <div className="slider-buttons">
-          <button>
+          <button onClick={prevSlide}>
             <i className="bi bi-chevron-left"></i>
           </button>
-          <button>
+          <button onClick={nextSlide}>
             <i className="bi bi-chevron-right"></i>
           </button>
         </div>
         <div className="slider-dots">
-          <button className="slider-dot active">
+          <button
+            className={`slider-dot ${currentSlide === 0 ? "active" : ""}`}
+            onClick={() => setCurrentSlide(0)}
+          >
             <span></span>
           </button>
-          <button className="slider-dot">
+          <button
+            className={`slider-dot ${currentSlide === 1 ? "active" : ""}`}
+            onClick={() => setCurrentSlide(1)}
+          >
             <span></span>
           </button>
-          <button className="slider-dot">
+          <button
+            className={`slider-dot ${currentSlide === 2 ? "active" : ""}`}
+            onClick={() => setCurrentSlide(2)}
+          >
             <span></span>
           </button>
         </div>
