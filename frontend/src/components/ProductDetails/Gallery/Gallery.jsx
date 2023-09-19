2c34c4841b59eb7ba0c67b5a76181747e20e4156
eminbasbayan <emin.basbayan@icloud.com>
2023-09-19 16:29:01 +0300

    gallery slider

--- frontend/src/components/ProductDetails/Gallery/Gallery.jsx
+++ frontend/src/components/ProductDetails/Gallery/Gallery.jsx
@@ -1,49 +1,97 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
+import Slider from "react-slick";
 import productsData from "../../../data.json";
 import "./Gallery.css";
 
+function PrevBtn({ onClick }) {
+  return (
+    <button
+      className="glide__arrow glide__arrow--left"
+      data-glide-dir="<"
+      onClick={onClick}
+      style={{
+        zIndex: "2",
+      }}
+    >
+      <i className="bi bi-chevron-left"></i>
+    </button>
+  );
+}
+
+function NextBtn({ onClick }) {
+  return (
+    <button
+      className="glide__arrow glide__arrow--right"
+      data-glide-dir=">"
+      onClick={onClick}
+      style={{
+        zIndex: "2",
+      }}
+    >
+      <i className="bi bi-chevron-right"></i>
+    </button>
+  );
+}
+
+NextBtn.propTypes = {
+  onClick: PropTypes.func,
+};
+
+PrevBtn.propTypes = {
+  onClick: PropTypes.func,
+};
+
 const Gallery = () => {
-  const [activeImg, setActiveImg] = useState(productsData[0].img.singleImage);
+  const [activeImg, setActiveImg] = useState({
+    img: productsData[0].img.singleImage,
+    imgIndex: 0,
+  });
+
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+  };
 
   return (
     <div className="product-gallery">
       <div className="single-image-wrapper">
-        <img src={activeImg} id="single-image" alt="" />
+        <img src={activeImg.img} id="single-image" alt="" />
       </div>
       <div className="product-thumb">
         <div className="glide__track" data-glide-el="track">
           <ol className="gallery-thumbs glide__slides">
-            {productsData[0].img.thumbs.map((itemImg, index) => (
-              <li
-                className="glide__slide glide__slide--active"
-                key={index}
-                onClick={() => setActiveImg(itemImg)}
-              >
-                <img
-                  src={itemImg}
-                  alt=""
-                  className={`img-fluid ${
-                    itemImg === activeImg ? "active" : ""
-                  } `}
-                />
-              </li>
-            ))}
+            <Slider {...sliderSettings}>
+              {productsData[0].img.thumbs.map((itemImg, index) => (
+                <li
+                  className="glide__slide glide__slide--active"
+                  key={index}
+                  onClick={() =>
+                    setActiveImg({
+                      img: productsData[0].img.thumbs[index],
+                      imgIndex: index,
+                    })
+                  }
+                >
+                  <img
+                    src={itemImg}
+                    alt=""
+                    className={`img-fluid ${
+                      activeImg.imgIndex === index
+                        ? "active"
+                        : ""
+                    } `}
+                  />
+                </li>
+              ))}
+            </Slider>
           </ol>
         </div>
-        <div className="glide__arrows" data-glide-el="controls">
-          <button
-            className="glide__arrow glide__arrow--left"
-            data-glide-dir="<"
-          >
-            <i className="bi bi-chevron-left"></i>
-          </button>
-          <button
-            className="glide__arrow glide__arrow--right"
-            data-glide-dir=">"
-          >
-            <i className="bi bi-chevron-right"></i>
-          </button>
-        </div>
+        <div className="glide__arrows" data-glide-el="controls"></div>
       </div>
     </div>
   );
