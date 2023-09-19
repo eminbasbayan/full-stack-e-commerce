cd928cf8b065ea3b14f095e700598cac761310af
eminbasbayan <emin.basbayan@icloud.com>
2023-09-19 20:16:27 +0300

    sepet localStorage işlemleri

--- frontend/src/context/CartProvider.jsx
+++ frontend/src/context/CartProvider.jsx
@@ -1,12 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
 
-  console.log("cart items:", cartItems);
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = (cartItem) => {
     // setCartItems([...cartItems, cartItem]); 1. yol
