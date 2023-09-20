4f296bb7d02c11e105d0b008cd212a69ad35ec0c
eminbasbayan <emin.basbayan@icloud.com>
2023-09-20 20:38:11 +0300

    mongoDb kurulumu

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const port = 5000;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://emin-e-commerce:Xu63EREZP61cSKyS@mern-e-commerce.ixovwma.mongodb.net/"
+    );
+    console.log("Connected to mongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Hello Express.js");
 });
 
-app.get("/api", (req, res)=> {
-  res.send("This is API Route.") 
-})
+app.get("/api", (req, res) => {
+  res.send("This is API Route.");
+});
 
-app.listen(5000,()=> {
-    console.log(`Sunucu ${5000} portunda çalışıyor.`);
-})
+app.listen(port, () => {
+  connect();
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
