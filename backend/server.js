bd26a5259fcb389516a431edbddcbb975c98dfe5
eminbasbayan <emin.basbayan@icloud.com>
2023-09-20 20:08:41 +0300

    express.js kurulumu

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
-const a = 5;
+const express = require("express");
+const app = express();
 
-console.log(a);
+app.get("/", (req, res) => {
+  res.send("Hello Express.js");
+});
+
+app.get("/api", (req, res)=> {
+  res.send("This is API Route.") 
+})
+
+app.listen(5000,()=> {
+    console.log(`Sunucu ${5000} portunda çalışıyor.`);
+})
