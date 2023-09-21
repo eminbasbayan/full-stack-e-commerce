9673038e5ddf25f27400e75051de9efc7ceede86
eminbasbayan <emin.basbayan@icloud.com>
2023-09-21 23:19:50 +0300

    random avatar oluşturma

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User.js");
 
+const generateRandomAvatar = () => {
+  const randomAvatar = Math.floor(Math.random() * 71);
+  return `https://i.pravatar.cc/300?img=${randomAvatar}`;
+};
+
+
 // Kullanıcı Oluşturma (Create - Register)
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const defaultAvatar = generateRandomAvatar();
 
     const existingUser = await User.findOne({ email });
 
@@ -22,6 +29,7 @@ router.post("/register", async (req, res) => {
       username,
       email,
       password: hashedPassword,
+      avatar: defaultAvatar,
     });
 
     await newUser.save();
