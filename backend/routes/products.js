7b679a870b4316e622dde9748423df0b04588a69
eminbasbayan <emin.basbayan@icloud.com>
2023-09-22 01:10:41 +0300

    product güncelleme

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -43,4 +43,29 @@ router.get("/:productId", async (req, res) => {
   }
 });
 
+// Ürün güncelleme (Update)
+router.put("/:productId", async (req, res) => {
+    try {
+      const productId = req.params.productId;
+      const updates = req.body;
+  
+      const existingProduct = await Product.findById(productId);
+  
+      if (!existingProduct) {
+        return res.status(404).json({ error: "Product not found." });
+      }
+  
+      const updatedProduct = await Product.findByIdAndUpdate(
+        productId,
+        updates,
+        { new: true }
+      );
+  
+      res.status(200).json(updatedProduct);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
+
 module.exports = router;
