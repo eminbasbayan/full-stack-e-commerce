159032124a0ec57d1688cc91668c2fb7037f3a38
eminbasbayan <emin.basbayan@icloud.com>
2023-09-22 01:44:00 +0300

    coupon getirme

--- backend/routes/coupons.js
+++ backend/routes/coupons.js
@@ -4,16 +4,71 @@ const Coupon = require("../models/Coupon.js");
 
 // Yeni bir kupon oluşturma (Create)
 router.post("/", async (req, res) => {
-    try {
-      const newCoupon = new Coupon(req.body);
-      await newCoupon.save();
-  
-      res.status(201).json(newCoupon);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Server error." });
+  try {
+    const { code } = req.body;
+
+    const existingCoupon = await Coupon.findOne({ code });
+
+    if (existingCoupon) {
+      return res.status(400).json({ error: "This coupon is alread exists." });
     }
-  });
 
+    const newCoupon = new Coupon(req.body);
+    await newCoupon.save();
 
-  module.exports = router
+    res.status(201).json(newCoupon);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Tüm kuponları getirme (Read - All)
+router.get("/", async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+
+    res.status(200).json(coupons);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Belirli bir kuponu getirme (Read - Single by Coupon ID)
+router.get("/:couponId", async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+
+    const coupon = await Coupon.findById(couponId);
+
+    if (!coupon) {
+      return res.status(404).json({ error: "Coupon not found." });
+    }
+
+    res.status(200).json(coupon);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Belirli bir kuponu getirme (Read - Single by Coupon Code)
+router.get("/code/:couponCode", async (req, res) => {
+  try {
+    const couponCode = req.params.couponCode;
+
+    const coupon = await Coupon.findOne({ code: couponCode });
+
+    if (!coupon) {
+      return res.status(404).json({ error: "Coupon not found." });
+    }
+    const { discountPercent } = coupon;
+    res.status(200).json({ discountPercent });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+module.exports = router;
