fc3aeb2d4f8ee8e31d7d30323f0cdb7be4fc3655
eminbasbayan <emin.basbayan@icloud.com>
2023-09-21 23:41:37 +0300

    kullanıcı giriş işlemi

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,6 @@ const generateRandomAvatar = () => {
   return `https://i.pravatar.cc/300?img=${randomAvatar}`;
 };
 
-
 // Kullanıcı Oluşturma (Create - Register)
 router.post("/register", async (req, res) => {
   try {
@@ -35,6 +34,33 @@ router.post("/register", async (req, res) => {
     await newUser.save();
 
     res.status(201).json(newUser);
+  } catch (error) {}
+});
+
+// Kullanıcı girişi (Login)
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid password." });
+    }
+
+    res.status(200).json({
+      id: user._id,
+      email: user.email,
+      username: user.username,
+      role: user.role,
+      avatar: user.avatar,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Server error." });
