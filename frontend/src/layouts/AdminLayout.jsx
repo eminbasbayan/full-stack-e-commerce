873dd3e118ba59326020d392c5e9048abaff0032
eminbasbayan <emin.basbayan@icloud.com>
2023-09-25 16:26:51 +0300

    ant design kurulum

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,6 +1,12 @@
+import { Button } from "antd";
+
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
+      <Button type="primary">Primary Button</Button>
+      <Button type="primary" danger>
+        Primary Button
+      </Button>
       <div>sidebar</div>
       <div>header</div>
       <div>content</div>
