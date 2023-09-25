e907b0c83b30cd6ae29c2e18a88830e2843700d7
eminbasbayan <emin.basbayan@icloud.com>
2023-09-25 16:49:05 +0300

    ant design ile layout oluşturma

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,17 +1,177 @@
-import { Button } from "antd";
+import { Layout, Menu } from "antd";
+import PropTypes from "prop-types";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  RollbackOutlined,
+  BarcodeOutlined,
+  DashboardOutlined,
+  ShoppingCartOutlined,
+  AppstoreOutlined,
+} from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
-const AdminLayout = () => {
+const { Sider, Header, Content } = Layout;
+
+const AdminLayout = ({ children }) => {
+  const navigate = useNavigate();
+
+  const menuItems = [
+    {
+      key: "1",
+      icon: <DashboardOutlined />,
+      label: "Dashboard",
+      onClick: () => {
+        navigate(`/admin`);
+      },
+    },
+    {
+      key: "2",
+      icon: <AppstoreOutlined />,
+      label: "Kategoriler",
+      path: "/",
+      children: [
+        {
+          key: "3",
+          label: "Kategori Listesi",
+          path: "/admin/categories",
+          onClick: () => {
+            navigate(`/admin/categories`);
+          },
+        },
+        {
+          key: "4",
+          label: "Yeni Kategori Oluştur",
+          path: "/admin/categories/create",
+          onClick: () => {
+            navigate("/admin/categories/create");
+          },
+        },
+      ],
+    },
+    {
+      key: "5",
+      icon: <LaptopOutlined />,
+      label: "Ürünler",
+      path: "/",
+      children: [
+        {
+          key: "6",
+          label: "Ürün Listesi",
+          path: "/admin/products",
+          onClick: () => {
+            navigate(`/admin/products`);
+          },
+        },
+        {
+          key: "7",
+          label: "Yeni Ürün Oluştur",
+          path: "/admin/products/create",
+          onClick: () => {
+            navigate("/admin/products/create");
+          },
+        },
+      ],
+    },
+    {
+      key: "8",
+      icon: <BarcodeOutlined />,
+      label: "Kuponlar",
+      path: "/admin/coupons",
+      children: [
+        {
+          key: "9",
+          label: "Kupon Listesi",
+          path: "/admin/coupons",
+          onClick: () => {
+            navigate(`/admin/coupons`);
+          },
+        },
+        {
+          key: "10",
+          label: "Yeni Kupon Oluştur",
+          path: "/admin/coupons/create",
+          onClick: () => {
+            navigate("/admin/coupons/create");
+          },
+        },
+      ],
+    },
+    {
+      key: "11",
+      icon: <UserOutlined />,
+      label: "Kullanıcı Listesi",
+      path: "/admin/users",
+      onClick: () => {
+        navigate(`/admin/users`);
+      },
+    },
+    {
+      key: "12",
+      icon: <ShoppingCartOutlined />,
+      label: "Siparişler",
+      onClick: () => {
+        navigate(`/admin/orders`);
+      },
+    },
+    {
+      key: "13",
+      icon: <RollbackOutlined />,
+      label: "Ana Sayfaya Git",
+      onClick: () => {
+        navigate(`/`);
+      },
+    },
+  ];
+
   return (
     <div className="admin-layout">
-      <Button type="primary">Primary Button</Button>
-      <Button type="primary" danger>
-        Primary Button
-      </Button>
-      <div>sidebar</div>
-      <div>header</div>
-      <div>content</div>
+      <Layout
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <Sider width={200} theme="dark">
+          <Menu
+            mode="vertical"
+            style={{
+              height: "100%",
+            }}
+            items={menuItems}
+          />
+        </Sider>
+        <Layout>
+          <Header>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                color: "white",
+              }}
+            >
+              <h2>Admin Paneli</h2>
+            </div>
+          </Header>
+          <Content>
+            <div
+              className="site-layout-background"
+              style={{
+                padding: "24px 50px",
+                minHeight: 360,
+              }}
+            >
+              {children}
+              asd
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 };
 
 export default AdminLayout;
+
+AdminLayout.propTypes = {
+  children: PropTypes.node,
+};
