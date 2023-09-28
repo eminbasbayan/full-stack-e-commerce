d1cbf2f02f557c1028016836eb7dfba3b086056e
eminbasbayan <emin.basbayan@icloud.com>
2023-09-28 16:55:26 +0300

    review form component ayarları

--- frontend/src/components/Reviews/ReviewForm.jsx
+++ frontend/src/components/Reviews/ReviewForm.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 const ReviewForm = () => {
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState("");
+
+  const handleRatingChange = (e, newRating) => {
+    e.preventDefault();
+    setRating(newRating);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      rating: rating,
+      review: review,
+    };
+
+    console.log(formData);
+  };
+
   return (
-    <form className="comment-form">
+    <form className="comment-form" onSubmit={handleSubmit}>
       <p className="comment-notes">
         Your email address will not be published. Required fields are marked
         <span className="required">*</span>
@@ -11,25 +31,45 @@ const ReviewForm = () => {
           <span className="required">*</span>
         </label>
         <div className="stars">
-          <a href="#" className="star">
+          <a
+            href="#"
+            className={`star ${rating === 1 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 1)}
+          >
             <i className="bi bi-star-fill"></i>
           </a>
-          <a href="#" className="star">
-            <i className="bi bi-star-fill"></i>
-            <i className="bi bi-star-fill"></i>
-          </a>
-          <a href="#" className="star">
-            <i className="bi bi-star-fill"></i>
-            <i className="bi bi-star-fill"></i>
-            <i className="bi bi-star-fill"></i>
-          </a>
-          <a href="#" className="star">
-            <i className="bi bi-star-fill"></i>
-            <i className="bi bi-star-fill"></i>
+          <a
+            href="#"
+            className={`star ${rating === 2 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 3)}
+          >
             <i className="bi bi-star-fill"></i>
             <i className="bi bi-star-fill"></i>
           </a>
-          <a href="#" className="star">
+          <a
+            href="#"
+            className={`star ${rating === 3 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 3)}
+          >
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+          </a>
+          <a
+            href="#"
+            className={`star ${rating === 4 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 4)}
+          >
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+          </a>
+          <a
+            href="#"
+            className={`star ${rating === 5 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 5)}
+          >
             <i className="bi bi-star-fill"></i>
             <i className="bi bi-star-fill"></i>
             <i className="bi bi-star-fill"></i>
@@ -43,21 +83,12 @@ const ReviewForm = () => {
           Your review
           <span className="required">*</span>
         </label>
-        <textarea id="comment" cols="50" rows="10"></textarea>
-      </div>
-      <div className="comment-form-author form-comment">
-        <label htmlFor="name">
-          Name
-          <span className="required">*</span>
-        </label>
-        <input id="name" type="text" />
-      </div>
-      <div className="comment-form-email form-comment">
-        <label htmlFor="email">
-          Email
-          <span className="required">*</span>
-        </label>
-        <input id="email" type="email" />
+        <textarea
+          id="comment"
+          cols="50"
+          rows="10"
+          onChange={(e) => setReview(e.target.value)}
+        ></textarea>
       </div>
       <div className="comment-form-cookies">
         <input id="cookies" type="checkbox" />
