f32eca75c8983402019426dc6a1a5af0b0192bc4
eminbasbayan <emin.basbayan@icloud.com>
2023-09-28 20:52:13 +0300

    product search route belirleme

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -85,4 +85,19 @@ router.delete("/:productId", async (req, res) => {
   }
 });
 
+// Ürünleri isme göre ara
+router.get("/search/:productName", async (req, res) => {
+  try {
+    const productName = req.params.productName;
+    const products = await Product.find({
+      name: { $regex: productName, $options: "i" },
+    });
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;
