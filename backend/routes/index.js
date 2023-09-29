9be0c2776b85996443a55d516b4dfa21bb166153
eminbasbayan <emin.basbayan@icloud.com>
2023-09-30 01:47:50 +0300

    stripe ödeme tamamlama

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,7 @@ const authRoute = require("./auth.js");
 const productRoute = require("./products.js");
 const couponRoute = require("./coupons.js");
 const userRoute = require("./users.js");
+const paymentRoute = require("./payment.js");
 
 // Her rotayı ilgili yol altında kullanıyoruz
 router.use("/categories", categoryRoute);
@@ -14,6 +15,7 @@ router.use("/auth", authRoute);
 router.use("/products", productRoute); 
 router.use("/coupons", couponRoute);
 router.use("/users", userRoute);
+router.use("/payment", paymentRoute);
 
 module.exports = router;
  
---
+++ backend/routes/payment.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+router.post("/", async (req, res) => {
+  const { products, user, cargoFee } = req.body;
+
+  const lineItems = products.map((product) => ({
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: product.name,
+      },
+      unit_amount: Math.round(product.price * 100),
+    },
+    quantity: product.quantity,
+  }));
+
+  if (cargoFee !== 0) {
+    lineItems.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: "Hızlı Kargo",
+        },
+        unit_amount: cargoFee * 100,
+      },
+      quantity: 1,
+    });
+  }
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.CLIENT_DOMAIN}/success`,
+    });
+
+    res.status(200).json({ id: session.id });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+module.exports = router;
